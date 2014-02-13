418157f3994669be3f281e90d0326cc58c5e6f8b
Stas Fomin <stas-fomin@yandex.ru>
2014-02-13 21:24:12 +0400

    move common parts outside

---
+++ CorpWikiSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+// Specific features for ROSALAB corpwiki installation
+
+require_once(dirname(__FILE__) . '/ServerSettings.php');
+
+require_once($IP.'/extensions/EtherPadAttached/EtherPadAttached.php');
+$wgEtherPadAttachedPadUrl  = "http://pad.rosalab.com/p/";
+$wgEtherPadAttachedCalcUrl = "http://calc.rosalab.ru/";
+$wgEtherPadAttachedDrawUrl = "http://draw.rosalab.ru/d/";
+
